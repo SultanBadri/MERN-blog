bccdbf25581df369a4445e6a7baa9609a552d98f
Sultan Badri <sultanbadri@berkeley.edu>
2021-12-28 19:45:29 -0800

    small variable name change for better clarity

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Set up mongoose connection
 const mongoose = require("mongoose");
-const mongoDB = process.env.MONGODB_URL_STRING;
+const mongoDB = process.env.MONGODB_URI;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
