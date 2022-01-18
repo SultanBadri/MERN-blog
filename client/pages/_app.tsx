84621a551ee7caf33387bd608fcd9cf055987254
Sultan Badri <sultanbadri@berkeley.edu>
2022-01-18 10:45:20 -0800

    small changes

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import Head from "next/head";
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>My Blog</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
