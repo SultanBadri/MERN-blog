68ab63d84b78ea5bb8ee5b8e22bfa9d6e5f017fb
Sultan Badri <sultanbadri@berkeley.edu>
2022-01-30 15:17:08 -0800

    testing tailwind

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,9 +1,12 @@
 import { useState, useEffect } from "react";
+import bloggingSVG from "../images/blogging.svg";
 
 function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <h1 className="text-3xl font-semibold text-center">
+        Welcome to my blog!
+      </h1>
     </div>
   );
 }
