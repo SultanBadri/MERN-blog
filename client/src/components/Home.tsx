e40d56622db566e6ff2fc9df806694118f987621
Sultan Badri <sultanbadri@berkeley.edu>
2022-01-30 16:40:19 -0800

    update designs of home page and nav

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -4,9 +4,19 @@ import bloggingSVG from "../images/blogging.svg";
 function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-semibold text-center">
-        Welcome to my blog!
-      </h1>
+      <div className="bg-gray-300 py-20 px-8">
+        <h1 className="text-5xl font-bold">Welcome to my blog!</h1>
+        <p className="pt-4 w-96 leading-6">
+          Hi there! Welcome to my blog, where I give my thoughts on full stack
+          technologies along with other things!
+        </p>
+        {/* <img
+        src={bloggingSVG}
+        alt="blog"
+        className="flex items-center justify-center"
+      /> */}
+      </div>
+      <h2 className="text-2xl mt-12 font-semibold">Blogs</h2>
     </div>
   );
 }
--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -1,18 +1,18 @@
 import { useState, useEffect } from "react";
-import { Link, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Nav() {
   return (
-    <div className="bg-purple-500 p-4 flex justify-between items-center">
+    <div className=" py-6 px-4 flex justify-between items-center drop-shadow-md">
       <Link to="/">
-        <h2 className="text-2xl">My Blog</h2>
+        <h2 className="text-3xl text-purple-600 font-semibold">My Blog</h2>
       </Link>
       <ul className="flex">
         <Link to="/login">
-          <li className="px-2">Log in</li>
+          <li className="px-2 duration-300 hover:text-purple-600">Log in</li>
         </Link>
         <Link to="/signup">
-          <li className="px-2">Sign up</li>
+          <li className="px-2 hover:text-purple-600">Sign up</li>
         </Link>
       </ul>
     </div>
